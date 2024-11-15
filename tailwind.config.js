adc21180ba8c8f56fc5bfbfd6d9d2a459f84b42c
aqeelsalsoft <aqeel.shahid@salsoft.net>
2024-11-16 02:45:20 +0500

    reverting tailwind purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,32 +8,27 @@ export default {
     "./app.vue",
     "./error.vue",
   ],
-  purge: [
-    './src/**/*.html',
-    './src/**/*.vue',
-    './src/**/*.jsx',
-  ],
   theme: {
     extend: {
       fontFamily: {
         headings: 'Quicksand, sans-serif', // Adds a new `font-headings` class
         description: 'Poppins, sans-serif', // Adds a new `font-text` class
       },
-      animation: {
-        // adjust speed according to your need
-        marquee: 'marquee 40s linear infinite',
-        marquee2: 'marquee 40s linear infinite',
-      },
-      keyframes: {
-        marquee: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(calc(-110px * 8))' },
-        },
-        marquee2: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(0%)' },
-        }
-      }
+      // animation: {
+      //   // adjust speed according to your need
+      //   marquee: 'marquee 40s linear infinite',
+      //   marquee2: 'marquee 40s linear infinite',
+      // },
+      // keyframes: {
+      //   marquee: {
+      //     '0%': { transform: 'translateX(100%)' },
+      //     '100%': { transform: 'translateX(calc(-110px * 8))' },
+      //   },
+      //   marquee2: {
+      //     '0%': { transform: 'translateX(100%)' },
+      //     '100%': { transform: 'translateX(0%)' },
+      //   }
+      // }
     },
   },
   plugins: [],
