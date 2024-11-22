03e1cbfa5de9a82c755924d1f16e4f672ba46ddf
aqeelsalsoft <aqeel.shahid@salsoft.net>
2024-11-22 22:54:58 +0500

    zoho chat widget code

--- components/General/ZohoScript/Wrapper.vue
+++ components/General/ZohoScript/Wrapper.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="zoho__script">
-        <script type="text/javascript" id="zsiqchat" async defer>
+        <!-- <script type="text/javascript" id="zsiqchat" async defer>
         var $zoho=$zoho || {};
         $zoho.salesiq = $zoho.salesiq || {
           widgetcode: "siq7554f5a6de5947a3bbb48ecbd8b715e435bf2a636fa2e2571eb445ab76f10a55",
@@ -15,6 +15,8 @@
         s.src="https://salesiq.zohopublic.com/widget";
         var t=d.getElementsByTagName("script")[0];
         t.parentNode.insertBefore(s,t);
-      </script>
+      </script> -->
+        <script type="text/javascript"
+            id="zsiqchat" async defer>var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode: "siq7554f5a6de5947a3bbb48ecbd8b715e435bf2a636fa2e2571eb445ab76f10a55", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.async=true;s.defer=true;s.src="https://salesiq.zohopublic.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);</script>
     </div>
 </template>
